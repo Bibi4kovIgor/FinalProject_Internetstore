2853d07bb64d172ce8c6e31314097d20128b09ce
ihor <work.bibi4kov@gmail.com>
2024-02-07 15:59:06 +0200

    Added Thymeleaf components

--- src/main/java/edu/lemon/internetstore/web/controller/ProductsController.java
+++ src/main/java/edu/lemon/internetstore/web/controller/ProductsController.java
@@ -1,7 +1,6 @@
 package edu.lemon.internetstore.web.controller;
 
 import edu.lemon.internetstore.service.ProductsService;
-import edu.lemon.internetstore.web.rest.ProductsRestApiController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
